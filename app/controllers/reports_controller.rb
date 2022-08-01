fbeb29902655ae055c669fb5d1dac366ff7b3fa6
anupatil2711 <anushkapat27@gmail.com>
2022-08-01 14:19:24 +0530

    third commit

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -13,14 +13,14 @@ class ReportsController < ApplicationController
 		    end
 	end
 	def city1
-#		debugger
+#debugger
 		sel_city_name = params[:city_name]
 		  require 'json'
 			@result = File.read("/Users/apple/Downloads/response.json")
  			@order_details = JSON.parse(@result) #a["orders"].class = array
-			@city1_details = []   #an empty array to store address
+			@city_details = []   #an empty array to store address
  			@order_details["orders"].each do |n| 	
- 				@city1_details << n["billing_address"] if n["billing_address"]["city"].include? sel_city_name
+ 				@city_details << n["billing_address"] if n["billing_address"]["city"].include? sel_city_name
  			end
 	end
 	def product
